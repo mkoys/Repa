85ccc31e63b87eebdea5333f45eb52e865f76f0b
mkoys <mkoys@protonmail.com>
2023-03-07 18:07:18 +0100

    Refactor of calendar

--- Client/public/components/Calendar.js
+++ Client/public/components/Calendar.js
@@ -32,12 +32,24 @@ export default class LabelInput extends BaseComponent {
 
         this.connected(async () => {
             await this.load;
+            const nextMonthElement = this.shadowRoot.querySelector(".nextMonth");
+            const previousMonthElement = this.shadowRoot.querySelector(".previousMonth");
+
+            nextMonthElement.addEventListener("click", () => this.setMonth(1));
+            previousMonthElement.addEventListener("click", () => this.setMonth(-1));
+
             this.updateDate();
         });
     }
 
     selectedUpdate(callback) {
         this.selectedUpdateCallback = callback;
+    }
+
+    setMonth(adder) {
+        this.date.setMonth(this.date.getMonth() + adder);
+        this.updateDate();
+        this.selected = [];
     }
 
     async attributeChangedCallback(name, oldValue, newValue) {
@@ -128,29 +140,18 @@ export default class LabelInput extends BaseComponent {
             this.selectedUpdateCallback("closed", this.selected[daySelectedIndex]);
             this.removeSelection(daySelectedIndex);
         } else if (this.range !== false && event.shiftKey) {
-            if (this.range == dayIndex) {
-                return this.range = false;
-            }
-
-            this.days[this.range].element.classList.remove("rangeBegin");
+            if (this.range == dayIndex) {return this.range = false}
 
             const start = this.range > dayIndex ? dayIndex : this.range;
             const end = this.range > dayIndex ? this.range : dayIndex;
             const rangeSelection = this.days.slice(start, end + 1);
 
-            rangeSelection[0].element.classList.add("rangeStart");
-            rangeSelection[rangeSelection.length - 1].element.classList.add("rangeEnd");
-            rangeSelection[0].element.children[0].classList.add("selected");
-            rangeSelection[rangeSelection.length - 1].element.children[0].classList.add("selected");
-
             for (let index = 0; index < rangeSelection.length; index++) {
                 const item = rangeSelection[index];
-
                 const isSelected = this.selected.findIndex(value => Array.isArray(value) ?
                     value.findIndex((value) => value.date.getTime() == item.date.getTime()) > -1 :
                     value.date.getTime() == item.date.getTime()
                 );
-
 
                 if (isSelected > -1) {
                     this.selectedUpdateCallback("closed", this.selected[isSelected]);
@@ -158,24 +159,19 @@ export default class LabelInput extends BaseComponent {
                 }
             }
 
-            for (let index = 1; index < rangeSelection.length - 1; index++) {
-                const item = rangeSelection[index];
-                item.element.children[0].classList.add("range");
-            }
-
+            this.renderRange(rangeSelection);
             this.selected.push(rangeSelection);
             this.selectedUpdateCallback("opened", rangeSelection);
-
             this.range = false;
         } else if (event.shiftKey) {
             this.days[dayIndex].element.classList.add("rangeBegin");
             this.range = dayIndex;
         } else if (event.ctrlKey) {
-            element.children[0].classList.add("selected");
+            this.renderDate(element);
             this.selected.push(this.days[dayIndex]);
             this.selectedUpdateCallback("opened", this.days[dayIndex]);
         } else if (!selected) {
-            element.children[0].classList.add("selected");
+            this.renderDate(element);
             this.selected.push(this.days[dayIndex]);
             this.selectedUpdateCallback("opened", this.days[dayIndex]);
         } else {
@@ -184,9 +180,27 @@ export default class LabelInput extends BaseComponent {
                 this.removeSelection(0);
             }
 
-            element.children[0].classList.add("selected");
+            this.renderDate(element);
             this.selected.push(this.days[dayIndex]);
             this.selectedUpdateCallback("opened", this.days[dayIndex]);
+        }
+    }
+
+    renderDate(selected) {
+        selected.children[0].classList.add("selected");
+    }
+
+    renderRange(selection) {
+        this.days[this.range].element.classList.remove("rangeBegin");
+
+        selection[0].element.classList.add("rangeStart");
+        selection[selection.length - 1].element.classList.add("rangeEnd");
+        this.renderDate(selection[0].element);
+        this.renderDate( selection[selection.length - 1].element);
+
+        for (let index = 1; index < selection.length - 1; index++) {
+            const item = selection[index];
+            item.element.children[0].classList.add("range");
         }
     }
 
