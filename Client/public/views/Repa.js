8b5013ab408eef7f03c03e8be3cbe09ec3279729
mkoys <mkoys@protonmail.com>
2023-03-08 17:28:42 +0100

    Attendance save

--- Client/public/views/Repa.js
+++ Client/public/views/Repa.js
@@ -20,35 +20,39 @@ export default class Repa extends BaseComponent {
         const userInfoElement = this.shadowRoot.querySelector("marble-usercard");
         const userInfo = await this.getUserInfo();
 
-        // attendance.save(async () => {
-        //     const data = attendance.getData();
-
-        //     console.log(data);
-
-        //     const response = await fetch(config.baseURL + "/repa/save", {
-        //         method: "POST",
-        //         body: JSON.stringify(data),
-        //         headers: {
-        //             "Content-type": "application/json",
-        //             "Authorization": "Bearer " + localStorage.getItem("token")
-        //         }
-        //     });
-
-        //     const result = await response.json();
-
-        //     console.log(result);
-        // });
-
+        
         userInfoElement.setAttribute("avatar", userInfo.avatar);
         userInfoElement.setAttribute("username", userInfo.username);
         userInfoElement.setAttribute("role", userInfo.role);
-
+        
         calendar.selectedUpdate((type, item) => {
             if (type === "opened") {
                 if (!Array.isArray(item)) {
                     const newAttendance = document.createElement("marble-attendance");
-                    newAttendance.setAttribute("date", item.date);
                     mainElement.appendChild(newAttendance);
+
+                    newAttendance.setAttribute("date", item.date);
+
+                    newAttendance.save(async () => {
+                        const data = newAttendance.getData();
+            
+                        console.log(data);
+            
+                        const response = await fetch(config.baseURL + "/repa/save", {
+                            method: "POST",
+                            body: JSON.stringify(data),
+                            headers: {
+                                "Content-type": "application/json",
+                                "Authorization": "Bearer " + localStorage.getItem("token")
+                            }
+                        });
+            
+                        const result = await response.json();
+            
+                        if(!result.error) {
+                            newAttendance.setAttribute("status", "Saved");
+                        }
+                    });
                 }
             }
 
