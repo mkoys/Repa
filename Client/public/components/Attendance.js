31c99ef254614bb964df00fc34fbee2a15ffa90f
mkoys <mkoys@protonmail.com>
2023-03-08 09:54:21 +0100

    Attendace attributes

--- Client/public/components/Attendance.js
+++ Client/public/components/Attendance.js
@@ -2,7 +2,7 @@ import BaseComponent from "../source/BaseComponent.js";
 
 export default class LabelInput extends BaseComponent {
 
-    static get observedAttributes() { return [] }
+    static get observedAttributes() { return ["status", "statusColor", "date"] }
 
     constructor() {
         super();
@@ -14,6 +14,22 @@ export default class LabelInput extends BaseComponent {
 
     async attributeChangedCallback(name, oldValue, newValue) {
         await this.load;
+        const statusElement = this.shadowRoot.querySelector(".status");
+        const dateElement = this.shadowRoot.querySelector(".date");
 
+        switch (name) {
+            case "statusColor":
+                statusElement.style.color = newValue;
+                break;
+            case "status":
+                statusElement.textContent = newValue;
+                break;
+            case "date":
+                dateElement.textContent = newValue;
+                break;
+
+            default:
+                break;
+        }
     }
 }
