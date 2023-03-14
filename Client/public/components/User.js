59895ce02d11d66eb5d9849d3e6cd6ad6caa4a83
mkoys <mkoys@protonmail.com>
2023-03-14 19:22:02 +0100

    User component

--- Client/public/components/User.js
+++ Client/public/components/User.js
@@ -2,7 +2,7 @@ import BaseComponent from "../source/BaseComponent.js";
 
 export default class LabelInput extends BaseComponent {
 
-    static get observedAttributes() { return ["username", "avatar", "class", "role", "rolecolor"] }
+    static get observedAttributes() { return ["username", "avatar", "class", "role", "rolecolor", "email", "submited", "accepted", "saved"] }
 
     constructor() {
         super();
@@ -14,7 +14,11 @@ export default class LabelInput extends BaseComponent {
 
     async attributeChangedCallback(name, oldValue, newValue) {
         await this.load;
+        const submitedElement = this.shadowRoot.querySelector(".submited");
+        const acceptedElement = this.shadowRoot.querySelector(".accepted");
+        const declinedElement = this.shadowRoot.querySelector(".declined");
         const usernameElement = this.shadowRoot.querySelector(".username");
+        const emailElement = this.shadowRoot.querySelector(".email");
         const avatarElement = this.shadowRoot.querySelector(".avatar");
         const classDetailsElement = this.shadowRoot.querySelector(".classDetails");
         const classElement = this.shadowRoot.querySelector(".class");
@@ -22,8 +26,20 @@ export default class LabelInput extends BaseComponent {
         const roleElement = this.shadowRoot.querySelector(".role");
 
         switch (name) {
+            case "submited":
+                submitedElement.children[0].textContent = newValue;
+                break;
+            case "accepeted":
+                acceptedElement.children[0].textContent = newValue;
+                break;
+            case "declined":
+                declinedElement.children[0].textContent = newValue;
+                break;
             case "username":
                 usernameElement.textContent = newValue;
+                break;
+            case "email":
+                emailElement.textContent = newValue;
                 break;
             case "avatar":
                 avatarElement.style.backgroundImage = `url(${newValue})`;
@@ -41,19 +57,19 @@ export default class LabelInput extends BaseComponent {
             case "role":
                 roleElement.textContent = newValue;
                 break;
-                case "rolecolor":
-                    switch (parseInt(newValue)) {
-                        case 1:
-                            roleDetailsElement.style.backgroundColor = "var(--success-color-lower-1)";
-                            roleElement.style.color = "var(--success-color-1)";
-                            break;
-                    
-                        default:
-                            roleDetailsElement.style.backgroundColor = "var(--main-color-lower-1)";
-                            roleElement.style.color = "var(--main-color-1)";
-                            break;
-                    }
-                    break;
+            case "rolecolor":
+                switch (parseInt(newValue)) {
+                    case 1:
+                        roleDetailsElement.style.backgroundColor = "var(--success-color-lower-1)";
+                        roleElement.style.color = "var(--success-color-1)";
+                        break;
+
+                    default:
+                        roleDetailsElement.style.backgroundColor = "var(--main-color-lower-1)";
+                        roleElement.style.color = "var(--main-color-1)";
+                        break;
+                }
+                break;
             default:
                 break;
         }
--- Client/public/views/Admin.js
+++ Client/public/views/Admin.js
@@ -9,5 +9,35 @@ export default class Repa extends BaseComponent {
         this.addStyle("/style.css");
         this.addStyle("/views/Admin.css");
         this.useTemplate("/views/Admin.html");
+
+        this.connected(async () => {
+            await this.load;
+            const listElement = this.shadowRoot.querySelector(".list");
+            const response = await (await fetch(config.baseURL + "/repa/users", {headers: {"Authorization": "Bearer " + localStorage.getItem("token")}})).json();
+            for(const user of response.users) {
+                const userComponent = document.createElement("marble-user");
+
+                userComponent.setAttribute("username", user.username);
+                userComponent.setAttribute("email", user.email);
+                userComponent.setAttribute("role", user.role);
+                userComponent.setAttribute("submited", user.submited);
+                userComponent.setAttribute("accepted", user.accepted);
+                userComponent.setAttribute("declined", user.declined);
+
+                if(user.avatar && user.avatar !== "") {
+                    userComponent.setAttribute("avatar", user.avatar);
+                }
+
+                if(user.role === "admin") {
+                    userComponent.setAttribute("roleColor", "1");
+                }
+
+                userComponent.addEventListener("click", () => {
+
+                });
+
+                listElement.appendChild(userComponent);
+            }
+        });
     }
 }
