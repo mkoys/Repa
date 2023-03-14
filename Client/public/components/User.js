cb30da46d64d7600b3f5e65b7b3185005042c56e
mkoys <mkoys@protonmail.com>
2023-03-14 16:05:36 +0100

    User component

--- Client/public/components/User.js
+++ Client/public/components/User.js
@@ -2,7 +2,7 @@ import BaseComponent from "../source/BaseComponent.js";
 
 export default class LabelInput extends BaseComponent {
 
-    static get observedAttributes() { return [] }
+    static get observedAttributes() { return ["username", "avatar", "class", "role", "rolecolor"] }
 
     constructor() {
         super();
@@ -14,9 +14,46 @@ export default class LabelInput extends BaseComponent {
 
     async attributeChangedCallback(name, oldValue, newValue) {
         await this.load;
-        switch (name) {
-            case "":
+        const usernameElement = this.shadowRoot.querySelector(".username");
+        const avatarElement = this.shadowRoot.querySelector(".avatar");
+        const classDetailsElement = this.shadowRoot.querySelector(".classDetails");
+        const classElement = this.shadowRoot.querySelector(".class");
+        const roleDetailsElement = this.shadowRoot.querySelector(".roleDetails");
+        const roleElement = this.shadowRoot.querySelector(".role");
 
+        switch (name) {
+            case "username":
+                usernameElement.textContent = newValue;
+                break;
+            case "avatar":
+                avatarElement.style.backgroundImage = `url(${newValue})`;
+                break;
+            case "class":
+                if (newValue) {
+                    console.log(1);
+                    classDetailsElement.style.visibility = "visible";
+                    classElement.textContent = newValue;
+                } else {
+                    classDetailsElement.style.visibility = "hidden";
+                    classElement.textContent = "";
+                }
+                break;
+            case "role":
+                roleElement.textContent = newValue;
+                break;
+                case "rolecolor":
+                    switch (parseInt(newValue)) {
+                        case 1:
+                            roleDetailsElement.style.backgroundColor = "var(--success-color-lower-1)";
+                            roleElement.style.color = "var(--success-color-1)";
+                            break;
+                    
+                        default:
+                            roleDetailsElement.style.backgroundColor = "var(--main-color-lower-1)";
+                            roleElement.style.color = "var(--main-color-1)";
+                            break;
+                    }
+                    break;
             default:
                 break;
         }
